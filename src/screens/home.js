ea21514d73e5bf55c4f7d081d726be97000034fc
zuenda25 <zuenda25@example.com>
2021-09-23 00:06:54 +0700

    Add screen

---
+++ src/screens/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import type { Node } from 'react';
+import { Text } from 'react-native';
+
+// const styles = StyleSheet.create({
+
+// });
+
+const Home: () => Node = () => (
+  <Text>Ini adalah Halaman Home</Text>
+);
+
+export default Home;
