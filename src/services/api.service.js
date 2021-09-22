16709de028ddee771fdab31255bf4c31da003466
zuenda25 <zuenda25@example.com>
2021-09-23 00:07:01 +0700

    API

---
+++ src/services/api.service.js
@@ -0,0 +1,39 @@
+import http from '../configs/http-common';
+
+const GET = async (path) => {
+  const res = await http.get(`${path}`);
+
+  return res.data;
+};
+
+const POST = async (path, params) => {
+  const res = await http.post(`${path}`, params);
+
+  return res.data;
+};
+
+const PUT = async (path, params) => {
+  const res = await http.put(`${path}`, params);
+
+  return res.data;
+};
+
+const DELETE = async (path) => {
+  const res = await http.delete(`${path}`);
+
+  return res;
+};
+
+const getContacts = () => GET('/contact');
+const addContact = (params) => POST('/contact', params);
+const updateContact = (id, params) => PUT(`/contact/${id}`, params);
+const deleteContact = (id) => DELETE(`/contact/${id}`);
+
+const restApi = {
+  getContacts,
+  addContact,
+  updateContact,
+  deleteContact,
+};
+
+export default restApi;
